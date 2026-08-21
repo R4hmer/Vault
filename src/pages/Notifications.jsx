0356408b5b748b4cc752c6d57832ca262d28d161
Halima <h4limaabdi@gmail.com>
2026-08-21 04:37:30 +0300

    feat: build notifications page

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,9 +1,22 @@
-import './Vault.css'
+import './Notifications.css'
 
 function Notifications() {
   return (
-    <main className="page">
-      <h1>Notifications page</h1>
+    <main className="notifications-page">
+      <div className="notifications-content">
+        <div className="notification-icon">♡</div>
+
+        <h1>NOTIFICATIONS</h1>
+
+        <p>Your notifications are waiting.</p>
+
+        <span>
+          Log in to see who favourited your ideas, liked or disliked your
+          ideas, and sent you messages.
+        </span>
+
+        <button>Log In</button>
+      </div>
     </main>
   )
 }
